fc1e2450e143b672074206efb18e6f5bdaf88c12
Tal Kirshboim <tal.kirshboim@gmail.com>
2016-08-31 11:22:48 +0200

    Added example code

---
+++ app/src/main/java/com/kirshboim/snackbardurationfalsewarning/MainActivity.java
@@ -0,0 +1,24 @@
+package com.kirshboim.snackbardurationfalsewarning;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        View container = findViewById(R.id.container);
+
+        createSnackbar(container, "Make Android support annotations great again!",
+                Snackbar.LENGTH_LONG).show();
+    }
+    @NonNull
+    private Snackbar createSnackbar(View view, String message, @Snackbar.Duration int duration) {
+        return Snackbar.make(view, message, duration);
+    }
+}
